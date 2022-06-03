3adba47ec10333bf8a2437f6204609988ab1558d
Ilya Che <nyrealman@gmail.com>
2022-06-03 14:47:52 +0300

    New

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Chakra, { CThemeProvider, CReset } from '@chakra-ui/vue'
+import "./scss/styles.scss"
+
+Vue.use(Chakra)
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(CThemeProvider, [h(CReset), h(App)])
+}).$mount()
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+      <c-button width="100px" class="button-default focus-remove button-scale" variant="link">Button
+            </c-button>
   </div>
 </template>
+
+
+<script>
+import { CButton } from "@chakra-ui/vue";
+
+export default {
+    components: {
+        CButton
+    }
+}
+</script>
